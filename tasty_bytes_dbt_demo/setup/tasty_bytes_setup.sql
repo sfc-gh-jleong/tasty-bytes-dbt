7e44f8e6695d7126a77ea12d4dd40228573cb768
James Leong <james.leong@snowflake.com>
2026-04-17 10:54:52 -0500

    Update tasty_bytes_setup.sql

--- tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
+++ tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
@@ -4,6 +4,13 @@
 CREATE OR REPLACE DATABASE tb_101; -- source
 CREATE OR REPLACE SCHEMA tb_101.raw;
 
+ALTER SCHEMA tasty_bytes_dbt_db.dev SET LOG_LEVEL = 'INFO';
+ALTER SCHEMA tasty_bytes_dbt_db.dev SET TRACE_LEVEL = 'ALWAYS';
+ALTER SCHEMA tasty_bytes_dbt_db.dev SET METRIC_LEVEL = 'ALL';
+
+ALTER SCHEMA tasty_bytes_dbt_db.prod SET LOG_LEVEL = 'INFO';
+ALTER SCHEMA tasty_bytes_dbt_db.prod SET TRACE_LEVEL = 'ALWAYS';
+ALTER SCHEMA tasty_bytes_dbt_db.prod SET METRIC_LEVEL = 'ALL';
 
 CREATE OR REPLACE FILE FORMAT tb_101.public.csv_ff 
 type = 'csv';
